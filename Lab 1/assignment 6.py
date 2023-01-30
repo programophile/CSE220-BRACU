51717f4e12de75acf7dc16f440981845c58b4051
Sayem- A ProgramoPhile <98210531+programophile@users.noreply.github.com>
2023-01-30 20:25:55 +0600

    Removeall

--- Lab 1/assignment 6.py
+++ Lab 1/assignment 6.py
@@ -1,7 +1,13 @@
-def removeAll(source,size,element) :
-    for i in range(size):
-        if source[i]==element :
-            source[i]=0
-    print(source)
+def remove_all(source, element):
+  b=0
+  new_arr = [0]* len(source)
+  for i in range(len(source)):
+        if source[i]!=element :
+            new_arr[b]=source[i]
+            b+=1
+
+
+
+  print(new_arr)
 source=[10,2,30,2,50,2,2,0,0]
-removeAll(source,7,2)
+remove_all(source,2)
