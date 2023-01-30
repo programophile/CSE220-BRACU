182f5fa2941b5df5e16c28a3035bdb48ad25f655
Sayem- A ProgramoPhile <98210531+programophile@users.noreply.github.com>
2023-01-30 20:33:15 +0600

    shift right

--- Lab 1/assignment 3.py
+++ Lab 1/assignment 3.py
@@ -1,6 +1,6 @@
-def shiftRight(source,k) :
+def shift_right(source,k) :
     for i in range(k,len(source)) :
         source[i]=source[i-k]
         source[i-k]=0
-    print(source)
-shiftRight([10,20,30,40,50,60],3)
+    return source
+print(shift_right([10,20,30,40,50,60],3))
