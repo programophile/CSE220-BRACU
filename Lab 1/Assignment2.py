a5028980681cc4206451721ae55535cf665c9121
Sayem- A ProgramoPhile <98210531+programophile@users.noreply.github.com>
2023-01-30 20:32:02 +0600

    rotate left

--- Lab 1/Assignment2.py
+++ Lab 1/Assignment2.py
@@ -1,9 +1,9 @@
-def rotateLeft(source,k) :
+def rotate_left(source,k) :
     b = 0
     for i in range(k,len(source)) :
         temp=source[i-k]
         source[i-k]=source[i]
         source[len(source)-k+b]=temp
         b+=1
-    print(source)
-rotateLeft([10,20,30,40,50,60],3)
+    return source
+print(rotate_left([10,20,30,40,50,60],3))
